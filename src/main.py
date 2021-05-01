7685efeb39ae2a3c695a952f613247444b817b33
Ryanseanlee <71867199+Ryanseanlee@users.noreply.github.com>
2021-05-01 12:35:41 -0700

    changing file upload
    changing file upload

--- src/main.py
+++ src/main.py
@@ -85,20 +85,18 @@ model = keras.models.load_model('./model')
 model.summary()
 
 
-uploaded = files.upload()
 
-for fn in uploaded.keys():
- 
-  # predicting images
-  path = '/content/' + fn
-  img = image.load_img(path, target_size=(150, 150))
-  x = image.img_to_array(img)
-  x = np.expand_dims(x, axis=0)
+# predicting images
+path = "download.jpg"#path to user image input that needs to be classified 
+img = image.load_img(path, target_size=(150, 150))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
 
-  images = np.vstack([x])
-  classes = model.predict(images, batch_size=10)
-  print(classes[0])
-  if classes[0]>0.5:
-    print(fn + " is a dog")
-  else:
-    print(fn + " is a cat")
+images = np.vstack([x])
+classes = model.predict(images, batch_size=10)
+print(classes[0])
+if classes[0]>0.5:
+      print(path + " is a dog")
+else:
+      print(path + " is a cat")
+
